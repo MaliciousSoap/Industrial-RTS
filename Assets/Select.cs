a1c6447633cb068560fc75cfbf08b22c4e6cf06b
Andy Phu <orangebookair@gmail.com>
2022-05-07 17:03:04 -0500

    dealing with shader issues

---
+++ Assets/Select.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour
+{
+    private Color initialColor;
+    private Color hoverColorMod = new Color(100, 100, 100, 0);
+    private Color selectColorMod = new Color(13, 13, 13, 0);
+
+    private bool selected = false;
+    private bool clicked = false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        initialColor = gameObject.GetComponent<Renderer>().material.color;
+    }
+
+    private void OnMouseOver()
+    {
+        if (clicked == false)
+        {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", initialColor - hoverColorMod);
+        }
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        clicked = !clicked;
+        gameObject.GetComponent<Renderer>().material.SetColor("_Color", initialColor - selectColorMod);
+        print("cliquer");
+    }
+
+    private void OnMouseDown()
+    {
+    }
+
+    private void OnMouseExit()
+    {
+        if (!clicked)
+        {
+            gameObject.GetComponent<Renderer>().material.color = initialColor;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
