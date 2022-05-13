7da41c699525ccc6d28195226ea8e9eec5964ef7
Andy Phu <orangebookair@gmail.com>
2022-05-12 20:08:43 -0500

    Json files are now viable as save files

--- Assets/GenerateMap.cs
+++ Assets/GenerateMap.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using System;
 
+[ExecuteAlways]
 public class GenerateMap : MonoBehaviour
 {
-    [ExecuteAlways]
-    private TextAsset file = Resources.Load("data") as TextAsset;
+    [Serializable]
+    public class Cell
+    {
+        public string id;
+        public string terrain;
+        public string building;
+    }
 
-    private items = JsonUtility.FromJson<ItemArray>(file.text);
+    [Serializable]
+    public class MapData
+    {
+        public Cell[] CellData;
+    }
+
+    private TextAsset file;
 
     private void OnDrawGizmos()
     {
-        print("legs");
     }
 
-    private void Start()
+    private void Awake()
     {
+        file = Resources.Load("MapData") as TextAsset;
+        print(file.text);
+        MapData json = JsonUtility.FromJson<MapData>(file.text);
+        Debug.Log(json.CellData);
+
+        foreach (Cell cell in json.CellData)
+        {
+            print(cell.id);
+        }
     }
 
     // Update is called once per frame
