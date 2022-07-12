9022095e98dfa4827e9d442b2d1a186fd6f52ea6
Andy <Orangebookair@gmail.com>
2022-07-12 21:23:11 +0700

    Cambodia

--- Assets/AddTiles.cs
+++ Assets/AddTiles.cs
@@ -7,33 +7,66 @@ public class AddTiles : MonoBehaviour
 {
     private bool pressed = false;
     public GameObject tile;
-    private GameObject tileCopy;
+    public GameObject tilePreview;
     private Button button;
+   
+
+
+    private Vector3 rawContactPoint;
+    private Vector3 cookedContactPoint; 
+
+    float snapValue(float value, float snap)
+    {
+
+        return (value - (value % snap) + Mathf.Round((value % snap) / snap) * snap);
+    }
+
     // Start is called before the first frame update
     void Start()
     {       
-        print(gameObject.GetComponent<Button>().colors.normalColor);
+        //print(gameObject.GetComponent<Button>().colors.normalColor);
         button = gameObject.GetComponent<Button>();
         button.onClick.AddListener(TaskOnClick);
+      
         
     }
 
+    public void Build()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(mouseRay, out RaycastHit info))
+        {
+
+            if (info.collider.gameObject.name == "Plane")
+            {
+                //                    print(info.point);
+                Instantiate(tile, new Vector3(snapValue(info.point.x, 10), 0, snapValue(info.point.z, 10)),Quaternion.identity);
+
+
+            }
+            else
+            {
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if (pressed)
-        {
+        {    
             Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(mouseRay,out RaycastHit info))
             {
-                print(info.collider.gameObject.name);
+               
                 if (info.collider.gameObject.name == "Plane")
                 {
-                    print("hitting");
+//                    print(info.point);
+                    cookedContactPoint = new Vector3(snapValue(info.point.x, 10), 0, snapValue(info.point.z, 10));
+                    print(cookedContactPoint);
+                    tilePreview.transform.position = cookedContactPoint;
                 }
                 else
                 {
-                    print("ahhhh");
                 }
             }
         }
@@ -45,11 +78,11 @@ public class AddTiles : MonoBehaviour
 
         if (pressed)
         {
-            tileCopy = Instantiate(tile);
+           // tilePreview = Instantiate(tile);
         }
         else
         {
-            Destroy(tileCopy);
+            tilePreview.transform.position = new Vector3(0, -69, 0);
         }
 
 
